b91378925fa0e67e7751605aaf8969587d6db636
pi3_etoile <pi3_etoile@orange.fr>
2018-07-31 22:47:46 +0200

    [elm] make login page prettier

--- frontend/src/pages/LoginPage.elm
+++ frontend/src/pages/LoginPage.elm
@@ -1,6 +1,6 @@
 module LoginPage exposing (loginPage)
 
-import Html exposing (Html, a, div, h2, i, input, p, span, text)
+import Html exposing (Html, a, div, h1, h2, i, input, p, span, text)
 import Html.Attributes exposing (class, for, placeholder, style, type_, value)
 import Types exposing (Model)
 import Msgs exposing (Msg(SendLogin, SetEmail, SetPassword))
@@ -9,15 +9,14 @@ import Html.Events exposing (onClick, onInput)
 
 loginPage : Model -> Html Msg
 loginPage model =
-    div [] [ h2 [] [ text "Login" ]
-           , div [ class "columns"]
-                 [  div [ class "column" ] []
-                 , div [ class "column" ] [ emailInput model
-                                          , passwordInput model
-                                          , submitLoginButton ]
-                 , div [ class "column" ] []
-                 ]
-           ]
+    div [ class "columns"]
+        [  div [ class "column" ] []
+        , div [ class "column" ] [ h1 [ class "is-size-1 has-text-link has-text-centered has-text-weight-light padding-bottom"] [ text "la super directrice, c'est toi !"]
+                                 , emailInput model
+                                 , passwordInput model
+                                 , submitLoginButton ]
+        , div [ class "column" ] []
+        ]
 
 emailInput : Model -> Html Msg
 emailInput model =
@@ -40,4 +39,6 @@ passwordInput model =
 
 submitLoginButton: Html Msg
 submitLoginButton =
-    a [ class "button is-info is-rounded is-block", onClick SendLogin ] [ text "Se connecter"]
+    div [ class "has-text-centered"]
+        [ a [ class "button is-info is-rounded", onClick SendLogin ] [ text "Se connecter"]
+        ]
